1872abde4b68054ab984c2eec7c3c9ff538fa8f2
inbn6619 <inbn6619@gmail.com>
2022-10-17 12:43:20 +0900

    Counter test ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Profile from "./Profile";
+// import Profile from "./Profile";
+import Counter from "./Counter.js";
 
 function App() {
   return (
     <div className='App'>
-      <Profile username = 'inbn6619' name='조의영' />
+      {/* <Profile username = 'inbn6619' name='조의영' /> */}
+      <Counter />
       learn react
     </div>
   );
